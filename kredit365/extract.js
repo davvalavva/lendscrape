e3bb50fdd5e6b2cb72407791164309a543aa1942
David Jonsson <dj1975@pm.me>
2018-10-27 00:22:34 +0200

    now Airbnb style (w. additional rules)

--- kredit365/extract.js
+++ kredit365/extract.js
@@ -1,8 +1,9 @@
-module.exports = async function () {
-    const result = await frame.evaluate(() => {
-        const myElements = document.querySelectorAll('.bar')
-        Array.from(myElements).forEach(doSomethingWithEachElement)
-        // return something
-    });
-    return result
-};
+module.exports = async function extract(frame) {
+  const result = await frame.evaluate(() => {
+    const myElements = document.querySelectorAll('.bar')
+    Array.from(myElements).forEach([])
+    // return something
+  })
+
+  return result
+}
--- kredit365/index.js
+++ kredit365/index.js
@@ -1,54 +1,38 @@
-const TEST = false
-const extract = require('./extract')
 const puppeteer = require('puppeteer')
-const {TimeoutError} = require('puppeteer/Errors')
-const browserAgents = require('browser-agents') // https://www.npmjs.com/package/browser-agents
-const SOURCES = ['https://www.kredit365.se/priser-p%C3%A5-l%C3%A5n']
+const { TimeoutError } = require('puppeteer/Errors')
+const browserAgents = require('browser-agents')
+const extract = require('./extract')
+
+const SOURCE = ['https://www.kredit365.se/priser-p%C3%A5-l%C3%A5n']
 
 const scrape = async () => {
-    const result
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    const frame = page.mainFrame()
-    page.setUserAgent(browserAgents.random())
-    page.setViewport({ width:1920, height:1080 })
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  const frame = page.mainFrame()
+  page.setUserAgent(browserAgents.random())
+  page.setViewport({ width: 1920, height: 1080 })
 
-    let i = 0, failure = false, retry = true
-    do {
-        try {
-            await frame.goto(SOURCES[i])
-            let extracted = await extract()
-    
-            browser.close()
-    
-            // TODO: Clean and transform data
+  try {
+    await frame.goto(SOURCE)
+    const extracted = await extract(frame)
+    browser.close()
 
-            return result
-    
-        } catch (error) {
-            if (error instanceof TimeoutError) {
-                // TODO
-                // failure = false|true
-                // retry   = false|true
-            } else {
-                // TODO
-                // failure = false|true
-                // retry   = false|true
-            }
-        }
-    } while (failure && retry)
-    
-    if (!failure) {
+    // TODO: Clean and transform data
+    const transformed = extracted
 
-
+    return transformed
+  } catch (error) {
+    if (error instanceof TimeoutError) {
+    // TODO
     } else {
-        // TODO: Handle total failure
+      // TODO
     }
-    
-    
-};
+  }
+
+  return {}
+}
 
 scrape().then((data) => {
-    console.log(data)
-    // TODO: Serialize and store data
+  global.console.log(data)
+  // TODO: Serialize and store data
 })
