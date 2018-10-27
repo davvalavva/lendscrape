1a83117e850f991a6a1328479ea5182c875ef0ef
David Jonsson <dj1975@pm.me>
2018-10-27 15:49:40 +0200

    first successful dataextraction

--- kredit365/extract.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function extract(frame) {
-  const result = await frame.evaluate(() => {
-    const myElements = document.querySelectorAll('.bar')
-    Array.from(myElements).forEach([])
-    // return something
-  })
-
-  return result
-}
--- kredit365/index.js
+++ kredit365/index.js
@@ -1,38 +1,50 @@
 const puppeteer = require('puppeteer')
 const { TimeoutError } = require('puppeteer/Errors')
 const browserAgents = require('browser-agents')
-const extract = require('./extract')
 
-const SOURCE = ['https://www.kredit365.se/priser-p%C3%A5-l%C3%A5n']
+try {
+  const scrape = async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    page.setUserAgent(browserAgents.random())
+    page.setViewport({ width: 1920, height: 1080 })
 
-const scrape = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  const frame = page.mainFrame()
-  page.setUserAgent(browserAgents.random())
-  page.setViewport({ width: 1920, height: 1080 })
+    await page.goto('https://www.kredit365.se/priser-p%C3%A5-l%C3%A5n')
 
-  try {
-    await frame.goto(SOURCE)
-    const extracted = await extract(frame)
+    const extracted = await page.evaluate(() => {
+      const expectedHeaders = ['belopp', 'uppl. avg', 'fakt. avg', 'ränta', 'total', 'eff. ränta', 'nom. ränta']
+      const headers = Array.from(document.querySelectorAll('.content > article > table > thead > tr > th'))
+
+      // Fånga upp om tabellrubriker förändrats eller bytt ordning och avbryt i så fall med Error
+      if (!headers.every((el, i) => el.innerText.trim().toLowerCase() === expectedHeaders[i])) {
+        throw new Error('Unexpected header(s)')
+      }
+
+      // Extrahera och typomvandla (String till Number) datan i HTML-tabellen till en 2D-array
+      return Array.from(document.querySelectorAll('.content > article > table > tbody > tr'))
+        .map(
+          row => Array.from(row.querySelectorAll('td'))
+            .map(el => el.innerText.trim().toLowerCase().match(/^\d\s{0,1}\d+/g))
+            .map(el => (el && el[0] ? parseInt(el[0].replace(/\s/ig, ''), 10) : null))
+        )
+    })
     browser.close()
 
-    // TODO: Clean and transform data
+    // Transformera datan till den struktur som för databasen
+    const storedKeys = ['belopp', 'uppl.avg', 'fakt.avg', 'ränta (kr)', 'betala totalt', 'eff. ränta (%)', 'nom. ränta (%)']
     const transformed = extracted
 
     return transformed
-  } catch (error) {
-    if (error instanceof TimeoutError) {
-    // TODO
-    } else {
-      // TODO
-    }
   }
 
-  return {}
+  scrape().then((data) => {
+    global.console.log(data)
+    // TODO: Serialize and store data
+  })
+} catch (error) {
+  if (error instanceof TimeoutError) {
+    throw error
+  } else {
+    throw error
+  }
 }
-
-scrape().then((data) => {
-  global.console.log(data)
-  // TODO: Serialize and store data
-})
