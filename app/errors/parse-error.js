4ccbf015cb2e69615cb9e25d477e9597e07639a1
David Jonsson <dj1975@pm.me>
2018-11-07 23:15:42 +0100

    parse-error.js remade

--- app/errors/parse-error.js
+++ app/errors/parse-error.js
@@ -1,34 +1,14 @@
-/**
- * @file Custom Error class for parsing errors
- * @copyright Copyright (C) David Jonsson - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * @author David Jonsson
- */
-
-/**
- * An error class for parse errors. Use only when no better suited subclass is found.
- *
- * @module errors/parse-error
- */
-
 const AbstractError = require('./abstract-error')
 
-/**
- * An general error class for parsing errors that have no better
- * suited alternative among its subclasses.
+/*
+ * An error representing a failure of some kind of parsing going on.
  *
- * @extends Error
+ * Arguments:
+ *    message        a non-empty and non-whitespace-only string with an error message
  */
 class ParseError extends AbstractError {
-  /**
-   * @param {string} message
-   * @param {string} fileName
-   */
-  constructor(...args) {
-    const [message, fileName] = args
-    const errorName = 'ParseError'
-    super(message, fileName, errorName)
+  constructor(message) {
+    super(message, { name: 'ParseError' })
   }
 }
 
--- app/errors/parse-error.test.js
+++ app/errors/parse-error.test.js
@@ -1,34 +1,18 @@
 /* eslint-disable no-new */
-/**
- * @file Tests for file {@link <install_folder>/errors/parse-error.js}
- * @copyright Copyright (C) David Jonsson - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * @author David Jonsson
- */
 
 const { test } = require('tap')
 const ParseError = require('./parse-error')
 
 test('ParseError(message, fileName)', (t) => {
-  t.type(new ParseError('message', 'file.js'), ParseError, `[01] Returns instance of ParseError when thrown with correctly set arguments`)
-  t.throws(() => { new ParseError() }, TypeError, `[02] Throws TypeError when no arguments given`)
-  t.throws(() => { new ParseError(null, 'file.js') }, TypeError, `[03] Throws TypeError when 1st argument is null`)
-  t.throws(() => { new ParseError(undefined, 'file.js') }, TypeError, `[04] Throws TypeError when 1st argument is undefined`)
-  t.throws(() => { new ParseError(301, 'file.js') }, TypeError, `[05] Throws TypeError when 1st argument is a Number`)
-  t.throws(() => { new ParseError([12, 13], 'file.js') }, TypeError, `[06] Throws TypeError when 1st argument is an Array`)
-  t.throws(() => { new ParseError({}, 'file.js') }, TypeError, `[07] Throws TypeError when 1st argument is an Object`)
-  t.throws(() => { new ParseError(() => {}, 'file.js') }, TypeError, `[08] Throws TypeError when 1st argument is a Function`)
-  t.throws(() => { new ParseError(Promise.resolve(1), 'file.js') }, TypeError, `[09] Throws TypeError when 1st argument is a Promise`)
-  t.throws(() => { new ParseError('', 'file.js') }, Error, `[10] Throws Error when 1st argument is given an empty string`)
-  t.throws(() => { new ParseError('message') }, TypeError, `[11] Throws TypeError when 2nd argument not given`)
-  t.throws(() => { new ParseError('message', null) }, TypeError, `[12] Throws TypeError when 2nd argument is null`)
-  t.throws(() => { new ParseError('message', undefined) }, TypeError, `[13] Throws TypeError when 2nd argument is undefined`)
-  t.throws(() => { new ParseError('message', 301) }, TypeError, `[14] Throws TypeError when 2nd argument is a Number`)
-  t.throws(() => { new ParseError('message', []) }, TypeError, `[15] Throws TypeError when 2nd argument is an Array`)
-  t.throws(() => { new ParseError('message', {}) }, TypeError, `[16] Throws TypeError when 2nd argument is an Object`)
-  t.throws(() => { new ParseError('message', () => {}) }, TypeError, `[17] Throws TypeError when 2nd argument is a Function`)
-  t.throws(() => { new ParseError('message', Promise.resolve(1)) }, TypeError, `[18] Throws TypeError when 2nd argument is a Promise`)
-  t.throws(() => { new ParseError('message', '') }, Error, `[19] Throws Error when 2nd argument is given an empty string`)
+  t.type(new ParseError('message'), ParseError, `[01] Returns instance of ParseError when thrown with correctly set arguments`)
+  t.throws(() => { new ParseError() }, ReferenceError, `[02] Throws ReferenceError when no arguments given`)
+  t.throws(() => { new ParseError(null) }, TypeError, `[03] Throws TypeError argument is null`)
+  t.throws(() => { new ParseError(301) }, TypeError, `[04] Throws TypeError when argument is a Number`)
+  t.throws(() => { new ParseError([12, 13]) }, TypeError, `[05] Throws TypeError when argument is an Array`)
+  t.throws(() => { new ParseError({}) }, TypeError, `[06] Throws TypeError when argument is an Object`)
+  t.throws(() => { new ParseError(() => {}) }, TypeError, `[07] Throws TypeError when argument is a Function`)
+  t.throws(() => { new ParseError(Promise.resolve(1)) }, TypeError, `[08] Throws TypeError when argument is a Promise`)
+  t.throws(() => { new ParseError('') }, Error, `[09] Throws Error when argument is an empty string`)
+  t.throws(() => { new ParseError('   ') }, Error, `[10] Throws Error when argument is a string with whitspaces only`)
   t.end()
 })
