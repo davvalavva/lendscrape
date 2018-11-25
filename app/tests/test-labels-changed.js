7d0ed70b8ab8ffbb86d9b82b33e3737b62dcf2f1
David Jonsson <dj1975@pm.me>
2018-11-26 00:13:36 +0100

    remade test file for labels-changed.js

--- app/tests/test-labels-changed.js
+++ app/tests/test-labels-changed.js
@@ -1,16 +1,7 @@
 const { test } = require('tap')
 const labelsChanged = require('../lib/labels-changed')
-const ValidationError = require('../errors/validation-error')
 
-const labels = [
-  'Belopp',
-  'Uppl. avg',
-  'Fakt. avg',
-  'Ränta',
-  'Total',
-  'Eff. ränta',
-  'Nom. ränta'
-]
+const labels = ['Belopp', 'Uppl. avg', 'Fakt. avg', 'Ränta', 'Total', 'Eff. ränta', 'Nom. ränta']
 const labelMap = [
   { field: 'amount', label: 'Belopp' },
   { field: 'contractingCost', label: 'Uppl. avg' },
@@ -21,40 +12,83 @@ const labelMap = [
   { field: 'interestRate', label: 'Nom. ränta' }
 ]
 
-test('labelsChanged(labels, labelMap)', (t) => {
-  t.type(labelsChanged(labels, labelMap), 'boolean', `[01] Returns a boolean when given valid arguments`)
+let badLabels
+let badLabelMap
 
-  t.equal(labelsChanged(labels, labelMap), false, `[02] Returns false when given valid arguments and labels haven't changed`)
+test('labelsChanged({ labels, labelMap })', (t) => {
+  t.throws(() => { labelsChanged() }, TypeError, `[01] Throws TypeError when called with no arguments`)
 
-  const changedLabels = [...labels]
-  changedLabels[2] = 'Aviavgift'
-  t.equal(labelsChanged(changedLabels, labelMap), true, `[03] Returns true when given valid arguments and labels have changed`)
+  t.throws(() => { labelsChanged({}) }, TypeError, `[02] Throws TypeError when called with an empty object`) // eslint-disable-line no-empty-pattern
 
-  t.throws(() => { labelsChanged() }, TypeError, `[04] Throws TypeError when called with no arguments`)
+  t.throws(() => { labelsChanged({ labels }) }, TypeError, `[03] Throws TypeError when called without property 'labelMap' in object passed`)
 
-  t.throws(() => { labelsChanged(labels) }, TypeError, `[05] Throws TypeError when called without second argument`)
+  t.throws(() => { labelsChanged({ labelMap }) }, TypeError, `[04] Throws TypeError when called without property 'labels' in object passed`)
 
-  t.throws(() => { labelsChanged({}, labelMap) }, TypeError, `[06] Throws TypeError when first argument isn't an array`)
+  badLabels = 'Belopp'
+  t.throws(() => { labelsChanged({ labels: badLabels, labelMap }) }, TypeError, `[05] Throws TypeError when property 'labels' is a string`)
 
-  t.throws(() => { labelsChanged(labels, {}) }, TypeError, `[07] Throws TypeError when second argument isn't an array`)
+  badLabels = 12
+  t.throws(() => { labelsChanged({ labels: badLabels, labelMap }) }, TypeError, `[06] Throws TypeError when property 'labels' is a number`)
 
-  const invalidLabels = [...labels]
-  invalidLabels[3] = 450
-  t.throws(() => {
-    labelsChanged(invalidLabels, labelMap)
-  }, ValidationError, `[08] Throws ValidationError when array in first argument contains non-string element`)
+  badLabels = {}
+  t.throws(() => { labelsChanged({ labels: badLabels, labelMap }) }, TypeError, `[07] Throws TypeError when property 'labels' is an object`)
 
-  const invalidLabelMap1 = [...labelMap]
-  invalidLabelMap1[0].additional_property = 'hello'
-  t.throws(() => { labelsChanged(labels, invalidLabelMap1) }, ValidationError, `[09] Throws ValidationError when given invalid array in second argument`)
+  badLabels = () => {}
+  t.throws(() => { labelsChanged({ labels: badLabels, labelMap }) }, TypeError, `[08] Throws TypeError when property 'labels' is a function`)
 
-  const invalidLabelMap2 = [...labelMap]
-  delete invalidLabelMap2[0].label
-  t.throws(() => { labelsChanged(labels, invalidLabelMap2) }, ValidationError, `[10] Throws ValidationError when given invalid array in second argument`)
+  badLabels = Promise.resolve(1)
+  t.throws(() => { labelsChanged({ labels: badLabels, labelMap }) }, TypeError, `[09] Throws TypeError when property 'labels' is a promise`)
 
-  const invalidLabelMap3 = [...labelMap]
-  invalidLabelMap3[4].label = ['Total']
-  t.throws(() => { labelsChanged(labels, invalidLabelMap3) }, ValidationError, `[11] Throws ValidationError when given invalid array in second argument`)
+  badLabels = true
+  t.throws(() => { labelsChanged({ labels: badLabels, labelMap }) }, TypeError, `[10] Throws TypeError when property 'labels' is a boolean`)
+
+  badLabels = null
+  t.throws(() => { labelsChanged({ labels: badLabels, labelMap }) }, TypeError, `[11] Throws TypeError when property 'labels' is null`)
+
+  badLabels = [...labels]
+  badLabels[0] = 12
+  t.throws(() => { labelsChanged({ labels: badLabels, labelMap }) }, TypeError, `[12] Throws TypeError when property 'labels' is an array having a number element`)
+
+  badLabelMap = 'Belopp'
+  t.throws(() => { labelsChanged({ labels, labelMap: badLabelMap }) }, TypeError, `[13] Throws TypeError when property 'labelMap' is a string`)
+
+  badLabelMap = 12
+  t.throws(() => { labelsChanged({ labels, labelMap: badLabelMap }) }, TypeError, `[14] Throws TypeError when property 'labelMap' is a number`)
+
+  badLabelMap = {}
+  t.throws(() => { labelsChanged({ labels, labelMap: badLabelMap }) }, TypeError, `[15] Throws TypeError when property 'labelMap' is an object`)
+
+  badLabelMap = () => {}
+  t.throws(() => { labelsChanged({ labels, labelMap: badLabelMap }) }, TypeError, `[16] Throws TypeError when property 'labelMap' is a function`)
+
+  badLabelMap = Promise.resolve(1)
+  t.throws(() => { labelsChanged({ labels, labelMap: badLabelMap }) }, TypeError, `[17] Throws TypeError when property 'labelMap' is a promise`)
+
+  badLabelMap = true
+  t.throws(() => { labelsChanged({ labels, labelMap: badLabelMap }) }, TypeError, `[18] Throws TypeError when property 'labelMap' is a boolean`)
+
+  badLabelMap = null
+  t.throws(() => { labelsChanged({ labels, labelMap: badLabelMap }) }, TypeError, `[19] Throws TypeError when property 'labelMap' is null`)
+
+  badLabelMap = [...labelMap]
+  badLabelMap[0] = { field: 'amount' }
+  t.throws(() => { labelsChanged({ labels, labelMap: badLabelMap }) }, TypeError, `[20] Throws TypeError when property 'labelMap' is an array having an object without property 'label'`)
+
+  badLabelMap = [...labelMap]
+  badLabelMap[0] = { field: 'amount', label: 12 }
+  t.throws(() => { labelsChanged({ labels, labelMap: badLabelMap }) }, TypeError, `[21] Throws TypeError when property 'labelMap' is an array having an object where property 'label' is a number`)
+
+  t.type(labelsChanged({ labels, labelMap }), 'boolean', `[22] Returns a boolean when given valid arguments`)
+
+  t.equal(labelsChanged({ labels, labelMap }), false, `[23] Returns false when given valid arguments and labels haven't changed`)
+
+  badLabels = [...labels]
+  badLabels[2] = 'Aviavgift'
+  t.equal(labelsChanged({ labels: badLabels, labelMap }), true, `[24] Returns true when given valid arguments and there exists a label not matching correspinding label in map object`)
+
+  badLabels = [...labels]
+  badLabels.pop()
+  t.equal(labelsChanged({ labels: badLabels, labelMap }), true, `[25] Returns true when nr of labels not equal to nr of labels in labelMap`)
 
   t.end()
 })
