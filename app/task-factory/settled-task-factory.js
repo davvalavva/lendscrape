e08e69c7ac3f5eb6f8f90b836ba16a1e5f9f4c9e
David Jonsson <dj1975@pm.me>
2018-11-27 06:37:39 +0100

    added test for settledTaskFactory()

--- app/task-factory/settled-task-factory.js
+++ app/task-factory/settled-task-factory.js
@@ -1,8 +1,15 @@
+const typeName = require('type-name')
 const { defaultMaxAttempts } = require('../config/runtime.json')
 
 module.exports = (task, err) => {
+  if (typeName(task) !== 'Object') {
+    throw TypeError(`Expected first argument to be an object, found type ${typeName(task)}`)
+  }
+  if (!(err instanceof Error)) {
+    throw TypeError(`Expected second argument to be an instance of Error, found type ${typeName(err)}. instanceof err === ${typeof err}`)
+  }
   const { isInteger: isInt, parseInt } = Number
-  const { response = {} } = err
+  const { response } = err
   const maxAttempts = task.maxAttempts || defaultMaxAttempts
   const code = response && isInt(response.statusCode) && parseInt(response.statusCode, 10)
   const noRetry = [400, 401, 403, 404, 405, 410, 412, 429]
