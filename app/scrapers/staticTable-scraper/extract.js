8937ba83058ce833ce8169f660a3da4436186e4b
David Jonsson <dj1975@pm.me>
2018-11-29 03:40:39 +0100

    new testfile test-staticTable-scraper-extract.js

--- app/scrapers/staticTable-scraper/extract.js
+++ app/scrapers/staticTable-scraper/extract.js
@@ -1,7 +1,14 @@
+const typeName = require('type-name')
 const cheerio = require('cheerio')
 const parseNum = require('../../lib/parse-number')
 
-module.exports = ({ html, hdSelector, trSelector }) => {
+module.exports = (config) => {
+  if (typeName(config) !== 'Object') throw new TypeError(`Argument must be an object, found type '${typeName(config)}'`)
+  if (typeName(config.html) !== 'string') throw new TypeError(`Property 'html' of argument must be a string, found type '${typeName(config.html)}'`)
+  if (typeName(config.hdSelector) !== 'string') throw new TypeError(`Property 'hdSelector' of argument must be a string, found type '${typeName(config.hdSelector)}'`)
+  if (typeName(config.trSelector) !== 'string') throw new TypeError(`Property 'trSelector' of argument must be a string, found type '${typeName(config.trSelector)}'`)
+
+  const { html, hdSelector, trSelector } = config
   const $ = cheerio.load(html)
   const elementContent = el => $(el).text().trim()
   const toStringsArray = nodes => nodes.toArray().map(elementContent)
--- app/tests/test-staticTable-scraper-extract.js
+++ app/tests/test-staticTable-scraper-extract.js
@@ -1,7 +1,68 @@
 const { test } = require('tap')
 const staticTableExtract = require('../scrapers/staticTable-scraper/extract.js')
 
+const html = `<!DOCTYPE html><html><body><table>
+<thead>
+  <tr>
+    <th>  Belopp</th>
+    <th>Total </th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>200 kr</td>
+    <td>  250 kr</td>
+  </tr>
+  <tr>
+    <td><strong> 3 000,00 kr</strong></td>
+    <td> 3 500 kr</td>
+  </tr>
+</tbody></table></body></html>`
+
+const hdSelector = 'table > thead > tr > th'
+const trSelector = 'table > tbody > tr'
+
 test('staticTableExtract({ html, hdSelector, trSelector })', (t) => {
-  // TODO
+  // [01] *****************************************************************************************
+  t.throws(() => { staticTableExtract() }, TypeError, `[01] Throws TypeError when called without any argument`)
+
+  // [02] *****************************************************************************************
+  t.throws(() => { staticTableExtract(null) }, TypeError, `[02] Throws TypeError when passed null`)
+
+  // [03] *****************************************************************************************
+  t.throws(() => { staticTableExtract([]) }, TypeError, `[03] Throws TypeError when passed an array`)
+
+  // [04] *****************************************************************************************
+  t.throws(() => { staticTableExtract(() => {}) }, TypeError, `[04] Throws TypeError when passed a function`)
+
+  // [05] *****************************************************************************************
+  t.throws(() => { staticTableExtract(Promise.resolve(1)) }, TypeError, `[05] Throws TypeError when passed a promise`)
+
+  // [06] *****************************************************************************************
+  t.throws(() => { staticTableExtract(true) }, TypeError, `[06] Throws TypeError when passed a boolean`)
+
+  // [07] *****************************************************************************************
+  t.throws(() => { staticTableExtract(12) }, TypeError, `[07] Throws TypeError when passed a number`)
+
+  // [08] *****************************************************************************************
+  t.throws(() => { staticTableExtract('12') }, TypeError, `[08] Throws TypeError when passed a string`)
+
+  // [09] *****************************************************************************************
+  t.throws(() => { staticTableExtract({ hdSelector, trSelector }) }, TypeError, `[09] Throws TypeError when missing 'html' property in passed object`)
+
+  // [10] *****************************************************************************************
+  t.throws(() => { staticTableExtract({ html: 1, hdSelector, trSelector }) }, TypeError, `[10] Throws TypeError when 'html' property isn't a string`)
+
+  // [11] *****************************************************************************************
+  t.throws(() => { staticTableExtract({ html, hdSelector: 1, trSelector }) }, TypeError, `[11] Throws TypeError when 'hdSelector' property isn't a string`)
+
+  // [12] *****************************************************************************************
+  t.throws(() => { staticTableExtract({ html, hdSelector, trSelector: 1 }) }, TypeError, `[12] Throws TypeError when 'trSelector' property isn't a string`)
+
+  // [13] *****************************************************************************************
+  const expected = { labels: ['Belopp', 'Total'], rows: [[200, 250], [3000, 3500]] }
+  const actual = staticTableExtract({ html, hdSelector, trSelector })
+  t.strictSame(actual, expected, `[13] Returns object with expected properties and values`)
+
   t.end()
 })
